14a3e1fcf58769ece0104b395870b0e14c6b7622
Ivan-Vankov <ivanvankov97@abv.bg>
2020-03-05 19:28:25 +0200

    Added code from the Unity Basics exercise

---
+++ Ivan/Unity Basics/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 3;
+
+	[SerializeField]
+	private BoxCollider boxCollider;
+
+	private void Start() {
+	}
+
+	void Update() {
+		Vector3 moveDirection =
+				new Vector3(Input.GetAxis("Horizontal"),
+							0,
+							Input.GetAxis("Vertical"))
+							.normalized
+				* Time.deltaTime
+				* speed;
+		Vector3 pointToLookAt = transform.position
+							  + moveDirection * 100;
+
+		transform.position += moveDirection;
+		transform.LookAt(pointToLookAt);
+	}
+}
