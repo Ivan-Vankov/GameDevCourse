e085c94f84de60b58760f6f82a5f10c40713c8ba
IvanVankov <i.vankov@c-trust.eu>
2020-03-06 15:29:11 +0200

    Added the base version for the third presentation

---
+++ Kamen/C# Basics Test/Assets/Scripts/TestSerializable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TestSerializable : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0, 10)]
+    private float testFloat;
+
+    [SerializeField]
+    private int testInt;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
