2fdb8c35c0d406eee6058885c46bfda4f80d29cc
Kamen Vakavchiev <kanitkameh@gmail.com>
2020-03-14 22:09:32 +0200

    Add demos and slides

---
+++ Kamen/Physics/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+            openDoor();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+            closeDoor();
+    }
+    void openDoor()
+    {
+        //door.transform.Translate(new Vector3(0, 3, 0));
+        //code below has interpolation for physics
+        door.GetComponent<Rigidbody>().MovePosition(door.transform.position + new Vector3(0,3,0));
+    }
+    void closeDoor()
+    {
+        //door.transform.Translate(new Vector3(0, -3, 0));
+        //code below has interpolation for physics
+        door.GetComponent<Rigidbody>().MovePosition(door.transform.position + new Vector3(0,-3,0));
+    }
+}
---
+++ Kamen/Physics/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody rb;
+    private Vector3 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+    void FixedUpdate()
+    {
+        rb.AddRelativeForce(speed*input);
+    }
+}
