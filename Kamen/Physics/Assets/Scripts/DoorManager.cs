22e711426877637fece689131d2ecb623c18255a
Kamen Vakavchiev <kanitkameh@gmail.com>
2020-03-15 02:24:17 +0200

    Smoother door movement

--- Kamen/Physics/Assets/Scripts/DoorManager.cs
+++ Kamen/Physics/Assets/Scripts/DoorManager.cs
@@ -6,26 +6,52 @@ public class DoorManager : MonoBehaviour
 {
     [SerializeField]
     GameObject door;
+    bool shouldOpen = false;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag.Equals("Player"))
-            openDoor();
+
+        if (other.tag.Equals("Player"))
+        {
+            shouldOpen = true;
+            StartCoroutine(openDoor());
+        }
     }
     private void OnTriggerExit(Collider other)
     {
+        
         if (other.tag.Equals("Player"))
-            closeDoor();
+        {
+            shouldOpen = false;
+            StartCoroutine(closeDoor());
+        }
+            
     }
-    void openDoor()
+    IEnumerator openDoor()
     {
-        //door.transform.Translate(new Vector3(0, 3, 0));
-        //code below has interpolation for physics
-        door.GetComponent<Rigidbody>().MovePosition(door.transform.position + new Vector3(0,3,0));
+        while (shouldOpen)
+        {
+            door.transform.Translate(Time.deltaTime * new Vector3(0, 3, 0));
+
+            if (door.transform.position.y >= 3.771)
+            {
+                door.transform.position = new Vector3(door.transform.position.x, 3.771f, door.transform.position.z);
+                yield break;
+            }
+            yield return null;
+        }
     }
-    void closeDoor()
+    IEnumerator closeDoor()
     {
-        //door.transform.Translate(new Vector3(0, -3, 0));
-        //code below has interpolation for physics
-        door.GetComponent<Rigidbody>().MovePosition(door.transform.position + new Vector3(0,-3,0));
+        while (!shouldOpen)
+        {
+            door.transform.Translate(Time.deltaTime * new Vector3(0, -3, 0));
+
+            if (door.transform.position.y <= 0.771)
+            {
+                door.transform.position = new Vector3(door.transform.position.x, 0.771f, door.transform.position.z);
+                yield break;
+            }
+            yield return null;
+        }
     }
 }
