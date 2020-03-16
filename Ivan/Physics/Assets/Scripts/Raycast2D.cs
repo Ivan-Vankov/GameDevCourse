73145dbfde210dead162897c6332d6f31952a7a1
Ivan-Vankov <ivanvankov97@abv.bg>
2020-03-17 01:28:54 +0200

    First of slides for the physics presentation

---
+++ Ivan/Physics/Assets/Scripts/Raycast2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast2D : MonoBehaviour {
+
+	void Start() {
+		Physics2D.queriesStartInColliders = false;
+	}
+
+	void Update() {
+
+		RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, 100);
+		if (hitInfo.collider != null) {
+			Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+		}
+		else {
+			Debug.DrawLine(transform.position, transform.position + transform.right * 100, Color.green);
+		}
+
+	}
+}
---
+++ Ivan/Physics/Assets/Scripts/RaycastForward.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastForward : MonoBehaviour {
+
+	public LayerMask mask;
+
+	void Update() {
+
+		Ray ray = new Ray(transform.position, transform.forward);
+		
+		if (Physics.Raycast(ray, 
+							out RaycastHit hitInfo, 
+							100, 
+							mask, 
+							QueryTriggerInteraction.Ignore)) {
+			print(hitInfo.collider.gameObject.name);
+			Destroy(hitInfo.transform.gameObject);
+			Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+		}
+		else {
+			Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+		}
+
+	}
+}
