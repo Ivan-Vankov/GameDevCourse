2072ea68e5a29ed75414a3a173f6cdd0593bb45a
Ivan-Vankov <ivanvankov97@abv.bg>
2020-03-19 19:05:15 +0200

    Fixed errors in scene

---
+++ Ivan/Physics/Assets/Scripts/PushUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PushUp : MonoBehaviour {
+
+	private Rigidbody body;
+
+	private void Start() {
+		body = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate() {
+		body.AddForce(Vector3.up * 10 * Time.fixedDeltaTime);
+		body.AddTorque(Vector3.up * 10 * Time.fixedDeltaTime);
+	}
+}
