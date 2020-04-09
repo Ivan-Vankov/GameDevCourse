576ec22842a13baf2703706a3af53c5ef9e33327
Ivan-Vankov <ivanvankov97@abv.bg>
2020-04-09 16:56:36 +0300

    Fixed Typos

--- Ivan/AI/Assets/Scripts/DieState.cs
+++ Ivan/AI/Assets/Scripts/DieState.cs
@@ -3,5 +3,6 @@
 public class DieState : StateMachineBehaviour {
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		animator.SetBool("IsDying", true);
+		animator.GetComponent<MovementController>().SetHorizontalMoveDirection(0);
 	}
 }
