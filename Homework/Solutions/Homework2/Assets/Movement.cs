05ab8eff8e802caf0a61ce9da06c6406fb9f2c71
Ivan-Vankov <ivanvankov97@abv.bg>
2020-04-12 18:37:00 +0300

    Fixed Homework 2 Solution

--- Homework/Solutions/Homework2/Assets/Movement.cs
+++ Homework/Solutions/Homework2/Assets/Movement.cs
@@ -2,60 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    [SerializeField]
-    float force;
-    bool isJumping;
-    Vector3 direction;
-    // Start is called before the first frame update
-    void Start()
-    {
-        isJumping = false;
-    }
+public class Movement : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        direction = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
-        if (Input.GetKey("space"))
-        {
-            //Debug.Log("space pressed");
-            isJumping = true;
-        }
-        if (Input.GetKeyUp("space"))
-        {
-            //Debug.Log("space not pressed");
-            isJumping = false;
-        }
-    }
-    void FixedUpdate()
-    {
-        if (isJumping)
-        {
-            int mask = LayerMask.GetMask("Default");
-            float rayLength = 0.7f;
-            float sphereRadius = 0.2f;
-            Vector3 origin = transform.position;
-            Vector3 direction = new Vector3(0,-1,0);
-            if (Physics.SphereCast(origin,
-                                sphereRadius,
-                                direction,
-                                out RaycastHit hit,
-                                rayLength,
-                                mask,
-                                QueryTriggerInteraction.Collide))
-            {
-                if(hit.collider.tag.Equals("Ground")){
-                    Debug.Log("Found ground");
-                    GetComponent<Rigidbody>().AddForce(new Vector3(0,1,0),ForceMode.Impulse);
-                }
-                else
-                {
-                    Debug.Log("No ground beneath us");
-                }
-            }
-        }
-        GetComponent<Rigidbody>().AddForce(direction*force);    
-    }
+	[SerializeField]
+	[Range(1, 30)]
+	private float speed = 10;
+
+	[SerializeField]
+	[Range(1, 30)]
+	private float jumpForce = 10;
+
+	private bool isJumping = false;
+	private Vector3 direction;
+
+	private new Rigidbody rigidbody;
+
+	private void Start() {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		if (Input.GetKey("space")) {
+			isJumping = true;
+		}
+		if (Input.GetKeyUp("space")) {
+			isJumping = false;
+		}
+	}
+
+	void FixedUpdate() {
+		if (isJumping) {
+			int mask = LayerMask.GetMask("Default");
+			float rayLength = 0.7f;
+			float sphereRadius = 0.2f;
+			Vector3 origin = transform.position;
+			Vector3 direction = new Vector3(0, -1, 0);
+			if (Physics.SphereCast(origin,
+								   sphereRadius,
+								   direction,
+								   out RaycastHit hit,
+								   rayLength,
+								   mask,
+								   QueryTriggerInteraction.Collide)
+				&& hit.collider.CompareTag("Ground")) {
+
+				rigidbody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+			}
+		}
+		rigidbody.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+	}
 }
