4cfa130edb320d77d4d3c41ac3ab84f3b7b82d65
Ivan-Vankov <ivanvankov97@abv.bg>
2020-04-29 21:14:54 +0300

    Added HUD

---
+++ Ivan/Coding Practice/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private Transform toFollow;
+    private Vector3 initialOffset;
+
+    public static Action<Vector3, Vector3> OnLookDirectionChanged;
+
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    public float moveSpeed = 1.0f;
+
+    public bool lockHeight = false;
+
+    float rotationY = 0F;
+
+    private void Start() {
+        initialOffset = transform.position - toFollow.position;
+        Cursor.visible = false;
+    }
+
+    void Update() {
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+        //Vector3 lookDirection = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y")).normalized;
+
+        Vector3 forwardVector = transform.TransformDirection(Vector3.forward);
+        Vector3 sidewaysVector = transform.TransformDirection(Vector3.right);
+        OnLookDirectionChanged?.Invoke(forwardVector, sidewaysVector);
+        transform.position = toFollow.position + initialOffset;
+        //if (lockHeight) {
+        //    var dir = transform.TransformDirection(new Vector3(xAxisValue, 0.0f, zAxisValue) * moveSpeed);
+        //    dir.y = 0.0f;
+        //    transform.position += dir;
+        //} else {
+        //    transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue) * moveSpeed);
+        //}
+    }
+}
---
+++ Ivan/Coding Practice/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0.5f, 3)]
+    private float speed = 2;
+
+    private NavMeshAgent agent;
+    private RaycastHit hitInfo = new RaycastHit();
+
+    private Vector3 forwardDirection = Vector3.forward;
+    private Vector3 sidewaysDirection = Vector3.right;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        //if (Input.GetMouseButton(0) && !Input.GetKey(KeyCode.LeftShift)) {
+        //    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    if (Physics.Raycast(ray, out hitInfo))
+        //        agent.destination = hitInfo.point;
+        //}
+
+        //Vector3 destination = transform.position;
+        //destination.z += Input.GetAxisRaw("Vertical") * speed;
+        //destination.x += Input.GetAxis("Horizontal") * speed;
+        agent.destination = transform.position 
+            + (forwardDirection  * Input.GetAxisRaw("Vertical")
+            + sidewaysDirection * Input.GetAxisRaw("Horizontal")).normalized;
+    }
+
+    private void OnEnable() {
+        CameraFollow.OnLookDirectionChanged += Move;
+    }
+
+    private void OnDisable() {
+        CameraFollow.OnLookDirectionChanged -= Move;
+    }
+
+    private void Move(Vector3 forwardDirection, Vector3 sidewaysDirection) {
+        this.forwardDirection = forwardDirection;
+        this.sidewaysDirection = sidewaysDirection;
+    }
+}
