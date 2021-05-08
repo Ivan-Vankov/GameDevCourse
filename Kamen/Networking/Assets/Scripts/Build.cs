6729d830d86417bf11e09b26e13e52497c9b0633
Kamen Vakavchiev <kanitkameh@gmail.com>
2021-05-08 09:20:08 +0300

    add rpc example and netvar slide

---
+++ Kamen/Networking/Assets/Scripts/Build.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.Messaging;
+
+public class Build : NetworkBehaviour
+{
+    public GameObject building;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    [ServerRpc]
+    void BuildServerRpc() {
+        Debug.Log("Server RPC called");
+        GameObject go = Instantiate(building,transform.position,Quaternion.identity);
+        go.GetComponent<NetworkObject>().Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && IsOwner) {
+            BuildServerRpc();
+        }
+    }
+}
